2e5bd386bb04243a8050d764eb9efc1d7f506048
Nachoman99 <Nacho@DESKTOP-8HIL9RT>
2020-04-25 10:30:13 -0600

    prueba2

--- prueba/prueba/Form1.Designer.cs
+++ prueba/prueba/Form1.Designer.cs
@@ -60,6 +60,8 @@
             this.checkBox30 = new System.Windows.Forms.CheckBox();
             this.checkBox31 = new System.Windows.Forms.CheckBox();
             this.checkBox32 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // checkBox1
@@ -382,11 +384,31 @@
             this.checkBox32.Text = "checkBox32";
             this.checkBox32.UseVisualStyleBackColor = true;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(104, 298);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 92);
+            this.button1.TabIndex = 32;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(367, 317);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(138, 72);
+            this.button2.TabIndex = 33;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.checkBox17);
             this.Controls.Add(this.checkBox18);
             this.Controls.Add(this.checkBox19);
@@ -460,6 +482,8 @@
         private System.Windows.Forms.CheckBox checkBox30;
         private System.Windows.Forms.CheckBox checkBox31;
         private System.Windows.Forms.CheckBox checkBox32;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
